a427035fd3072af5fee1e110f7c9aa1062f9e4bf
venkey-bhai <venkey-bhai@users.noreply.github.com>
2026-08-20 16:28:05 +0530

    Add Dockerfile for MVC frontend

--- Mymvcapp/obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ Mymvcapp/obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac553dff6cf108f32fd4dc4e3570ae6b3b3ce0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
